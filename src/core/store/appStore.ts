d105f6f0baa97f39210d459aac0e901ae96ec7c8
Aida Solat <aidasolatco@gmail.com>
2023-01-21 15:34:04 +0300

    app logic and structure

---
+++ src/core/store/appStore.ts
@@ -0,0 +1,18 @@
+//appStore.ts
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers/rootReducer';
+import rootSaga from '../sagas/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
